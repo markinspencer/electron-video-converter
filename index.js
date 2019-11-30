d40684d95d321ca95356571f85a49d6931834a86
Spencer Markin <smarkin@questica.com>
2019-11-30 14:12:05 -0500

    video metadata

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
@@ -12,4 +14,22 @@ app.on('ready', () => {
   });
 
   mainWindow.loadURL(`file:${__dirname}/src/index.html`);
+});
+
+ipcMain.on('videos:added', (e, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        if (err) reject(err);
+
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
+    });
+  });
+
+  Promise.all(promises)
+    .then(results => mainWindow.webContents.send('metadata:complete', results))
+    .catch(console.error);
 });
